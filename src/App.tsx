29b22306b39068e6404f8d6cb9d8f13068073074
anaikarsaad <anaikarsaad@gmail.com>
2024-02-13 20:26:15 -0500

    Add navigation, protected routing, onboarding screen, Button component,color picker

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,43 @@
 import React from 'react';
 import {
   createBrowserRouter,
+  createRoutesFromElements,
+  redirect,
+  Route,
   RouterProvider,
+  Routes,
 } from "react-router-dom";
 import Admin from './pages/Admin';
 import OnboardingPage from './pages/Onboarding-page';
 import SignUpPage from './pages/Sign-up-page';
 const App: React.FC = () => {
+const check:boolean = false;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+     <Route path='/' element={<Admin></Admin>}/>
+      <Route path='onboarding' element={<OnboardingPage/>}/>
+      <Route path='/signup' element={<SignUpPage/>}/>
+    <Route  
+    path='/admin'  
+    element={<Admin/>}
+     loader={async () => {
+      const user = await check
+      if (!check) {
+        throw redirect("/signup");
+      }
 
-  const router = createBrowserRouter([
-    {
-      path: "/signup",
-      element: <SignUpPage />,
-    },
-    {
-      path: "/onboarding",
-      element: <OnboardingPage />,
-    },
-    {
-      path: "/admin",
-      element: <Admin/>,
-    },
-  ]);
+      return "hello"
+    }}
+    
+    />
+    </Route>
+    
+    )
+  );
+ 
   return (
+    
     <RouterProvider router={router} />
     
   );
--- src/pages/Onboarding-page.tsx
+++ src/pages/Onboarding-page.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Button from '../components/Button';
+import { ColorPicker } from 'primereact/colorpicker';
 const OnboardingPage: React.FC = () => {
+  const[color,setColor]=useState("6466f1");
   return (
-    <div>
-        <h1>
-        Onboarding page
-        </h1>
+    <div className='flex items-center justify-center h-screen bg-[#F6F6F6] '>
+      <div className=' h-[552px] w-[556px] bg-[#FFFFFF] flex gap-3 items-center flex-col rounded-[20px] drop-shadow-2xl'>
+      <div className='pt-10 flex flex-col gap-1'>
+        <p className='font-bold text-2xl tracking-wide'>Lets setup some basic details</p>
+        <p className='self-center'>Fill the form to know better</p>
+      </div>
+      <input placeholder='Name' className='border rounded-[20px] border-[#B0B0B0] w-[438px] h-[52px]  p-3'/>
+      <input placeholder='Image Link' className='border rounded-[20px] border-[#B0B0B0] w-[438px] h-[52px] p-3'/>
+      <input placeholder='Color'  className='border rounded-[20px] border-[#B0B0B0] w-[438px] h-[52px] p-3'  />
+      <ColorPicker format="hex" value={color} onChange={(e) => setColor((e.value) as string)} className=''/>
+      <div className='pt-10'>
+      <Button buttonText='Submit'></Button>
+      
+      </div>
+      
+      </div>
         </div>
   );
 }
