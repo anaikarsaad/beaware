55ad2fae6c2c22cc10973751e88b2922ea8f2cba
Chandana Sree Sunkara <chandanasreesunkara@gmail.com>
2024-02-29 18:40:46 -0500

    Created YourImages Page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import OnboardingPage from './pages/Onboarding-page';
 import SignUpPage from './pages/Sign-up-page';
 import CompanionSetup from './pages/CompanionSetup';
 import ImageInfo from './pages/ImageInfo';
+import YourImages from './pages/YourImages';
 const App: React.FC = () => {
 const check:boolean = false;
   const router = createBrowserRouter(
@@ -24,6 +25,7 @@ const check:boolean = false;
       <Route path='/signup' element={<SignUpPage/>}/>
       <Route path='/cs' element={<CompanionSetup/>}/>
       <Route path="/ii" element={<ImageInfo name="Sample Image" imageUrl="https://via.placeholder.com/300" qrCodeData="Sample QR Code Data" />} />
+      <Route path='/your-images' element={<YourImages name="Sample Image" imageUrl="https://via.placeholder.com/300" />} />
         {/* ... (other routes) */}
     <Route  
     path='/admin'  
